403bcefb71d0df7f1ac32ea4475f66a7521456a2
Diego Alzugaray <Diegoalzugaray@gmail.com>
2024-08-11 10:11:43 -0400

    Initial Commit

---
+++ Level-Up-Supabase-v2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Level-Up-Supabase-v2
+//
+//  Created by Diego Alzugaray on 8/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Level-Up-Supabase-v2/Level_Up_Supabase_v2App.swift
@@ -0,0 +1,17 @@
+//
+//  Level_Up_Supabase_v2App.swift
+//  Level-Up-Supabase-v2
+//
+//  Created by Diego Alzugaray on 8/11/24.
+//
+
+import SwiftUI
+
+@main
+struct Level_Up_Supabase_v2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
